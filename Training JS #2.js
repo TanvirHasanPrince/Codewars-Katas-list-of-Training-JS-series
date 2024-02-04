8d55c75c08ec2b454b27a2b5d418a5a0eb5d1cf0
unknown <t.hasan.prince@gmail.com>
2024-02-04 20:46:57 +0600

    Training JS#2

--- Training JS #2.js
+++ Training JS #2.js
@@ -12,40 +12,31 @@ In the end, click "Submit" to submit your code pass this kata.
 */
 
 
-let v1 = 50,
-    v2 = 100,
-    v3 = 150,
-    v4 = 200,
-    v5 = 2,
-    v6 = 250;
-
-function equal1(){
-  let a = v1,   
-      b = v1;   
+var v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+function equal1() {
+  var a = v1;
+  var b = v1;
   return a + b;
 }
-
 //Please refer to the example above to complete the following functions
-function equal2(){
-  let a =  , //set number value to a
-      b =  ; //set number value to b
+function equal2() {
+  var a = v3;
+  var b = v1;
   return a - b;
 }
-
-function equal3(){
-  let a =  , //set number value to a
-      b =  ; //set number value to b
+function equal3() {
+  var a = v1;
+  var b = v5;
   return a * b;
 }
-
-function equal4(){
-  let a =  , //set number value to a
-      b =  ; //set number value to b
+function equal4() {
+  var a = v4;
+  var b = v5;
   return a / b;
 }
-
-function equal5(){
-  let a =  , //set number value to a
-      b =  ; //set number value to b
+function equal5() {
+  var a = v6;
+  var b = v3;
   return a % b;
 }
